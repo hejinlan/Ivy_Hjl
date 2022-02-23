7ef1be77801283ea630d3001bb53a984ccef973a
Hejinlan <70897155+hejinlan@users.noreply.github.com>
2022-02-23 22:02:02 +0800

    Add files via upload

---
+++ WinBat_TeraTerm/autoTeraTerm.bat
@@ -0,0 +1,5 @@
+"D:\Program Files (x86)\teraterm\ttpmacro.exe" "H:\WinBat_TeraTerm\autoTT.ttl" 
+"D:\Program Files (x86)\teraterm\ttpmacro.exe" "H:\WinBat_TeraTerm\autoTT.ttl" 
+"D:\Program Files (x86)\teraterm\ttpmacro.exe" "H:\WinBat_TeraTerm\autoTT.ttl" 
+"D:\Program Files (x86)\teraterm\ttpmacro.exe" "H:\WinBat_TeraTerm\autoTT.ttl" 
+exit
